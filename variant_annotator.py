51f8018bae54c38a99175a628cb2229f5f2f86da
Max Perez <oaklandmax@gmail.com>
2023-10-04 13:35:31 -0700

    updated the output to match spec.

--- variant_annotator.py
+++ variant_annotator.py
@@ -20,14 +20,6 @@ class VariantAnnotator:
         # TODO: check for stdin
         for line in sys.stdin:
             self.variant_list.append(line.strip())  # Remove trailing whitespace and return chars
-
-            
-    # def get_variants_from_file(self, filename_in) -> None:
-    #     # TODO: fix this data dir to be more local. very important
-    #     data_dir = '/home/max/python/myome/myome_challenge/'
-    #     with open(data_dir + filename_in) as my_file:
-    #         # self.variant_list = my_file.readlines()
-    #         self.variant_list = my_file.read().splitlines()  # removes newline and spaces
             
     def get_annotations(self, variant_in) -> dict:    
         url = 'https://rest.ensembl.org/vep/human/hgvs/' + variant_in
@@ -36,7 +28,6 @@ class VariantAnnotator:
         # Only process successful (200) responses
         if response.status_code == 200:
             annotation_json_data = response.json()[0]  # json object is returned in a one item list. just return the zero item for ease of reading
-            # print(json.dumps(annotation_json_data, indent=1))
 
             # Put data in a dict for later processing. Dict is good as we can reference by name later.
             # TODO: check for missing data? Not sure.
@@ -50,7 +41,7 @@ class VariantAnnotator:
                 'strand': annotation_json_data['strand'],
                 'gene_symbol': annotation_json_data['transcript_consequences'][0]['gene_symbol']
             }
-            print(variant_in, file=sys.stdout)
+            # print(variant_in, file=sys.stdout)
             return annotation_dict
         else:
             print('The input value: "' + variant_in + '" failed with a response of: "' + str(response.status_code) + '"', file=sys.stderr)
@@ -61,6 +52,7 @@ class VariantAnnotator:
         with open(self.filename_out, 'w', newline='') as tsvfile:
             writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
             writer.writerow(['variant', 'assembly_name', 'seq_region_name', 'start', 'end', 'most_severe_consequence', 'strand', 'genes'])
+            
 
     def process_results_to_file(self, annotation_in) -> None:
         # TODO: fix the path that this file gets written to
@@ -79,18 +71,20 @@ class VariantAnnotator:
                     annotation_in['gene_symbol']
                 ]
             )
-        
+    
+    def print_output_file_sdout(self):
+        with open(self.filename_out, 'r') as f:
+            print(f.read(), file=sys.stdout)
 
 ####################################################
 # Instantiate the Object  and Process the Variants #
 ####################################################
 
 v = VariantAnnotator()
-# v.get_variants_from_file('variants.txt')
-print('Processing Variants', file=sys.stdout)
 for variant in v.variant_list:
     annotation = v.get_annotations(variant)
     if annotation:
         v.process_results_to_file(annotation)
+v.print_output_file_sdout()
 print('Done Processing Variants, output written to ' + v.filename_out, file=sys.stdout)
 
